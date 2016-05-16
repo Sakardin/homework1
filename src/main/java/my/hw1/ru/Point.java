a2db60123323d895d644b0ca5965af0c134cfb84
Sakardin <sakardin@tut.by>
2016-05-16 09:40:53 +0300

    Записан тест рекодером

--- src/main/java/my/hw1/ru/Point.java
+++ src/main/java/my/hw1/ru/Point.java
@@ -12,5 +12,10 @@ class Point { double x, y;
 
     this.y = у;
 
-  } }
+  }
+
+//  public double Distance(Point p1, Point p2) {
+//
+//    return Math.sqrt( Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y-p2.y, 2) );
+}
 
