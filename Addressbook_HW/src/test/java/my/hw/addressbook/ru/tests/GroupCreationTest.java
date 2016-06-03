addf55b59eec70b1223d23c00ca36320d3bc6f94
Dmitry Sakardin <dsakardin@tula.co>
2016-06-03 13:07:47 +0300

    Дата Провайдер привязка к файлу

--- Addressbook_HW/src/test/java/my/hw/addressbook/ru/tests/GroupCreationTest.java
+++ Addressbook_HW/src/test/java/my/hw/addressbook/ru/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import my.hw.addressbook.ru.Model.Groups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,10 +16,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups(){
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-       list.add(new Object[]{new GroupData().withName("ter").withHeader("gfdgfdg").withFooter("fdgdfg")});
-       list.add(new Object[]{new GroupData().withName("ter2").withHeader("gfdgfdg2").withFooter("fdgdfg2")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+
         return list.iterator();
     }
 
@@ -26,7 +33,6 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation(GroupData group) {
             app.goTo().groupPage();
             Groups before = app.group().all();
-
             app.group().create(group);
             assertThat(app.group().count(), equalTo(before.size() + 1));
 
